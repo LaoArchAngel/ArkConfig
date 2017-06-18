8b9936813b315398d22921c2c8ef5e2dc4aea029
Lao <Lao@nexverse.net>
2017-06-18 21:03:11 +0000

    Apparently still need quotes

--- SetPassword.ps1
+++ SetPassword.ps1
@@ -7,4 +7,4 @@ Param(
 $setPass = Get-Content $passwordFile
 $token = "**SERVERPASSWORD**"
 
-(Get-Content .\GameUserSettings.ini)|ForEach-Object {$_ -replace $token,$setPass}|Set-Content .\GameUserSettings.ini
+(Get-Content .\GameUserSettings.ini)|ForEach-Object {$_ -replace "$token",$setPass}|Set-Content .\GameUserSettings.ini
