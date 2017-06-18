ebba47fe7f829bc96257d55141813a7d79ecc526
Lao <Lao@nexverse.net>
2017-06-18 19:04:54 +0000

    Created SetPassword powershell script

---
+++ SetPassword.ps1
@@ -0,0 +1,10 @@
+Param(
+	[ValidateScript({Test-Path -Path $_ -PathType Leaf})]
+	[string]
+	$passwordFile
+)
+
+$setPass = Get-Content $passwordFile
+$token = "**SERVERPASSWORD**"
+
+(Get-Content .\GameUserSettings.ini)|ForEach-Object {$_ -replace $token,$setPass}|Set-Content .\GameUserSettings.ini
