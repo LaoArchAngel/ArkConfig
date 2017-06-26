b531fc78066a3c00d1c8bca2a009bc0f9dd075a7
Lao <Lao@nexverse.net>
2017-06-26 20:03:41 +0000

    Changed RCONPort and trying to enable structure decay.

--- RunServer.cmd
+++ RunServer.cmd
@@ -5,4 +5,4 @@ git pull
 powershell .\SetPassword.ps1 C:\au-password.txt
 popd
 
-start "Asgaard Untamed" /normal "C:\arkserver\ShooterGame\Binaries\Win64\ShooterGameServer.exe" Ragnarok?listen?Port=7777?QueryPort=27015?MaxPlayers=100?AllowCrateSpawnsOnTopOfStructures=True -UseBattlEye -NotifyAdminCommandsInChat -useallavailablecores -usecache -nosteamclient -game -server -log
+start "Asgaard Untamed" /normal "C:\arkserver\ShooterGame\Binaries\Win64\ShooterGameServer.exe" Ragnarok?listen?Port=7777?QueryPort=27015?MaxPlayers=100?bRawSockets?AllowCrateSpawnsOnTopOfStructures=True -UseBattlEye -NotifyAdminCommandsInChat -nomansky -useallavailablecores -usecache -nosteamclient -game -server -log
